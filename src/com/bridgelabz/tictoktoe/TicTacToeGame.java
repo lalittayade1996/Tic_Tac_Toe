0993b879700bde2a423548c9b9ba1d66cce273ab
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-01 16:38:03 +0530

    Added : Tic-Tac-Toe Game UC-2

--- src/com/bridgelabz/tictoktoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictoktoe/TicTacToeGame.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.tictoktoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
-	char[] board = showBoard();
+	static char[] board = showBoard();
 
 	public static char[] showBoard() {
 		char[] board = new char[10];
@@ -13,8 +15,32 @@ public class TicTacToeGame {
 
 	}
 
+	static String Choice() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Just Choose Any One Mark For Start The Game");
+		System.out.println("       :X: & :O:        ");
+		String user = input.next();
+		System.out.println("Player Play's = " + user);
+
+		if (user.equals("X")) {
+			String computer = "O";
+			System.out.println("Computer Play's = " + computer);
+			return computer;
+		} else {
+			String computer = "X";
+			System.out.println("Computer Play's = " + computer);
+			return computer;
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome To Game ~Tic-Tac-Toe~ ");
+		System.out.println("Welcome To Game");
+		System.out.println("~Tic-Tac-Toe~");
+		System.out.println();
 		showBoard();
+		Choice();
+
 	}
 }
